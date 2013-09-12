ea5804765f3ac171948390dc1dec127ab86eefb2
Timo <timo@timo-Latitude-E6320.(none)>
2013-09-12 09:00:32 +0300

    homework2

--- homework.py
+++ homework.py
@@ -1,5 +1,27 @@
 
-print "abc". capitalize ()
-print "abc". center (12)
+print "abc123". capitalize ()
+
+print "abc123". center (12)
+
+print "abc123". count (1)
+ 
+print "abc123". decode ()
 
 
+print "abc123". endswith (20)
+
+print "abc123". expandtabs (4)
+
+print "abc123". find (0)
+
+print "abc123". index (1)
+
+print "abc123". isalnum (3)
+
+print "abc123". isalpha (6)
+
+print "abc123". isdigit (2)
+
+print "abc123". islower (2)
+
+print "abc123". isspace (1)
