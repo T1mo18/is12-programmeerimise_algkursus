b5223901be1dfdc0d54e7fb66c8dd37d9cdf4397
Timo <timo@timo-Latitude-E6320.(none)>
2013-09-24 21:15:22 +0300

    Kodused valemid

--- homework.py
+++ homework.py
@@ -1,27 +1,30 @@
+print "abc123". zfill (12)
 
 print "abc123". capitalize ()
 
 print "abc123". center (12)
 
-print "abc123". count (1)
+print "abc123". count ("21")
  
 print "abc123". decode ()
 
-
-print "abc123". endswith (20)
+print "abc123". endswith ("123")
 
 print "abc123". expandtabs (4)
 
-print "abc123". find (0)
+print "abc123". find ("b2")
 
-print "abc123". index (1)
+print "abc123". index ("1")
 
-print "abc123". isalnum (3)
+print "abc123". isalnum ()
 
-print "abc123". isalpha (6)
+print "abc123". isalpha ()
 
-print "abc123". isdigit (2)
+print "abc123". isdigit ()
 
-print "abc123". islower (2)
+print "Abc123". islower ()
 
-print "abc123". isspace (1)
+print "abc123". isspace ()
+
+
+
---
+++ valemid.py
@@ -0,0 +1,62 @@
+# s=a*h/2
+A = 5.0
+H =10.0
+S = A*H/2 
+print S
+
+# e=mc^2
+M = 10.0
+C = 1.8
+E = M*C*C
+print E
+
+
+# v=a^3
+A = 9.0
+V1 = A*A*A
+print V1
+
+# a^2+b^2=c^2
+A = 2.4
+B = 5.0
+C = (A*A)+(B*B)
+print C
+
+# p=2*(a+b)
+A = 2.5
+B = 18.0
+P = 2*(A+B)
+print P
+
+# sk=n*a*m/2
+N = 14.9
+A =  1.0
+M = 22.0
+SK= (N*A*M)/2
+print SK
+
+# v=abc
+A =5.9
+B =4.9
+C =3.9
+V = A*B*C
+print V
+
+# v=s/t
+S = 55.0
+T = 35.0
+V = S/T
+print V
+
+# i=u/r
+U = 76.0
+R = 23.0
+I = U/R
+print I
+
+# h=g*t^2/2 
+G = 10.0
+T =  1.0
+H = (G*(T*T))/2
+print H
+
